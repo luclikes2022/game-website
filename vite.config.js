1cf1950b20a790d1d2b49f54eda2e000e3a7b8dc
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-06-03 18:26:40 -0600

    Fixed not loading

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
+
 export default defineConfig({
     root: './',
     build: {
         rollupOptions: {
             input: {
-                home: '/src/pages/index.html',
-                about: '/src/pages/about.html',
-                content: '/src/pages/content.html',
-                projects: '/src/pages/projects.html'
+                home: './src/pages/index.html',
+                about: './src/pages/about.html',
+                content: './src/pages/content.html',
+                projects: './src/pages/projects.html'
             }
         }
-    }
+    },
+    server: {
+        open: '/src/pages/index.html', // Automatically opens the home page in the browser
+    },
+    base: './' // Ensure correct paths during build
 });
